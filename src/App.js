f83438be3a78b7d27b245da3736d8e36c7a3b2c9
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-16 15:57:47 +0530

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import ClickCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
-     
+      {/* <ClickCounter /> */}
+      <HookCounter />
     </div>
   );
 }
