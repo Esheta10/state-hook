30c5cee46b1b8766e193f33c341e087294b78c25
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-17 08:04:52 +0530

    run effects only once

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import HookCounterThree from "./components/HookCounterThree"
 import HookCounterFour from './components/HookCounterFour';
 import ClassCounterOne from './components/ClassCounterOne';
 import HookCounterOne from './components/HookCounterOne';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
@@ -17,7 +19,9 @@ function App() {
       {/* <HookCounterThree /> */}
       {/* <HookCounterFour /> */}
       {/* <ClassCounterOne />  */}
-      <HookCounterOne /> 
+      {/* <HookCounterOne />  */}
+      {/* <ClassMouse /> */}
+      <HookMouse />
     </div>
   );
 }
---
+++ src/components/ClassMouse.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class ClassMouse extends Component {
+  constructor(props) {
+    super(props);
+    // Initial state to store mouse co-ordinates
+    this.state = {
+      x: 0,
+      y: 0,
+    };
+  }
+  // This function updates state with current mouse co-ordinates
+  logMousePosition = e => {
+    this.setState({
+       x: e.clientX, // horizontal mouse position
+       y: e.clientY // vertical mouse position
+      });
+  }
+  // Runs when the component is first mounted to the DOM
+  componentDidMount() {
+    // Adding event listener to track mouse movement
+    window.addEventListener("mousemove", this.logMousePosition);
+  }
+  render() {
+    return (
+      <div>
+        X - {this.state.x} Y - {this.state.y}
+      </div>
+    );
+  }
+}
+
+export default ClassMouse;
